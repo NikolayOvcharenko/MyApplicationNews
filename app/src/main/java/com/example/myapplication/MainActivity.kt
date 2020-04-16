e79ff78dd1a86ffbe00edc52dedd1bee39f54222
Nikolay <56501319+NikolayOvcharenko@users.noreply.github.com>
2020-04-16 23:19:38 +0300

    2 step

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val vText = findViewById<TextView>(R.id.act1_text)
+        vText.setTextColor(0xFFFF0000.toInt())
+        vText.setOnClickListener {
+            Log.e("tag","Кнопка нажата")
+        }
+
+        Log.v("tag","запущен онкреате")
     }
 
     override fun onStart() {
