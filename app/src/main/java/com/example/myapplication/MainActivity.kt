c8601ca52493c1d3a2a28bc0ebfb9975e5e3acf0
Nikolay <56501319+NikolayOvcharenko@users.noreply.github.com>
2020-04-20 21:47:40 +0300

    2 step

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,17 +8,32 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var vText:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val vText = findViewById<TextView>(R.id.act1_text)
+        vText = findViewById<TextView>(R.id.act1_text)
         vText.setTextColor(0xFFFF0000.toInt())
         vText.setOnClickListener {
-            Log.e("tag","Кнопка нажата")
+            Log.e("tag", "Кнопка нажата")
+            val i = Intent(this, SecondActivity::class.java)
+            i.putExtra("tag1", vText.text)
+            startActivityForResult( i, 0)
         }
 
-        Log.v("tag","запущен онкреате")
+        Log.v("tag", "запущен онкреате")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(data!=null) {
+            val str = data.getStringExtra("tag2")
+
+            vText.text = str
+        }
     }
 
     override fun onStart() {
--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -1,12 +1,31 @@
 package com.example.myapplication
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.*
 
-class SecondActivity:Activity() {
+class SecondActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_activity)
+
+        val vEdit = findViewById<EditText>(R.id.act2_edit)
+
+        findViewById<Button>(R.id.act2_button).setOnClickListener {
+
+            val newStr = vEdit.text.toString()
+            val i = Intent()
+            i.putExtra("tag2", newStr)
+            setResult(0, i)
+            finish()
+        }
+
+        val str = intent.getStringExtra("tag1")
+
+        vEdit.setText(str)
     }
 }
