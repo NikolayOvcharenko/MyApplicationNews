b792c279fdc07964112d42f081e42b49e598c6d1
Nikolay <56501319+NikolayOvcharenko@users.noreply.github.com>
2020-05-03 11:40:06 +0300

    3 step https://stepik.org/lesson/67528/step/1?unit=44293

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.myapplication
 
+import android.app.ActivityManager
+import android.content.ClipDescription
 import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import com.google.gson.Gson
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.internal.operators.observable.ObservableRange
+import io.reactivex.schedulers.Schedulers
+import java.lang.RuntimeException
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var vText:TextView
+    lateinit var vText: TextView
+    var request: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +32,26 @@ class MainActivity : AppCompatActivity() {
         vText.setTextColor(0xFFFF0000.toInt())
         vText.setOnClickListener {
             Log.e("tag", "Кнопка нажата")
-            val i = Intent(this, SecondActivity::class.java)
-            i.putExtra("tag1", vText.text)
-            startActivityForResult( i, 0)
+            //val i = Intent(this, SecondActivity::class.java)
+            //i.putExtra("tag1", vText.text)
+            //startActivityForResult(i, 0)
+
+            val o =
+                createRequest("https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.bbci.co.uk%2Fnews%2Frss.xml")
+                    .map { Gson().fromJson(it, Feed::class.java) }
+                    //.flatMap { Observable.create<String> {} }.zipWith(Observable.create<String> {})
+                    //.map { it.second + it.first }
+                    .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+
+            request = o.subscribe({
+                
+                for(item in it.items)
+                    Log.w("Test", "Title: ${item.title}")
+ 
+            }, {
+                Log.e("test","",it)
+
+            })
         }
 
         Log.v("tag", "запущен онкреате")
@@ -29,7 +60,7 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(data!=null) {
+        if (data != null) {
             val str = data.getStringExtra("tag2")
 
             vText.text = str
@@ -53,6 +84,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onDestroy() {
+        request?.dispose()
         super.onDestroy()
     }
 }
+
+class Feed(
+    val items: ArrayList<FeedItem>
+)
+
+class FeedItem(
+    val title: String,
+    val link: String,
+    val thumbnail: String,
+    val description: String
+)
